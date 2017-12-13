065a6a64a051facae1903c15243b2bfc35435835
suzutatsu <tatsuhiko.suzuki@topgate.co.jp>
2017-12-13 10:05:32 +0900

    追加

--- brew.sh
+++ brew.sh
@@ -2,3 +2,6 @@ brew install git zsh curl wget tree ag jq peco gcc llvm go
 brew cask install adobe-reader
 brew cask install java
 brew install maven
+brew install npm
+brew install grunt
+brew install pip
